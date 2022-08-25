72a4e1df735e935b39a7976c5b97234296759133
chae03yb <chae03yb@gmail.com>
2022-08-25 23:32:50 +0900

    config.json 가져오기 수정, main.ts, util.ts: 오류 처리 수정 필요

--- main.ts
+++ main.ts
@@ -1,14 +1,7 @@
-import { Client } from "insta.js";
-import { OpenAPI, DatabaseIO } from "./util";
+import { Client } from "@androz2091/insta.js";
+import { OpenAPI, DatabaseIO, ServerError, NotFoundError, InvalidKey } from "./util";
 
-import { readFileSync } from "fs";
-
-// import { username, password } from "./config.json"; // FIXME: 가져오기 오류
-
-const config = JSON.parse(readFileSync("./config.json", "utf-8"));
-
-let username: string = config.username;
-let password: string = config.password;
+import { username, password } from "./config.json";
 
 const client: Client = new Client({ disableReplyPrefix: true });
 
@@ -19,16 +12,18 @@ client.on("connected", () => {
 client.on("messageCreate", msg => {
     if (msg.content?.startsWith("gsc ")) {
         let school_name: string = msg.content.split(" ")[1];
-        // if (gsc == "ERR_NF") // 오류 처리
         
         OpenAPI.getSchoolCode(school_name)
             .then(result => {
-                if (result === "ERR_NF")
+                msg.reply(`학교 코드: ${result}`);
+            }).catch(err => {
+                // FIXME: 오류 처리
+                if (err instanceof NotFoundError)
                     msg.reply("해당 학교를 찾을 수 없습니다");
-                else if (result === "ERR_SE")
-                    msg.reply("서버에 오류가 발생했습니다. 잠시 후에 다시 시도해주세요");
-                else
-                    msg.reply(`학교 코드: ${result}`);
+                if (err instanceof ServerError)
+                    msg.reply("API 서버에 오류가 발생했습니다. 잠시 후에 다시 시도해주세요");
+                if (err instanceof InvalidKey)
+                    msg.reply("API 키가 유효하지 않습니다.");
             });
     }
 });
--- util.ts
+++ util.ts
@@ -1,27 +1,42 @@
 import fetch from "node-fetch";
 
-import { readFileSync } from "fs";
-// import openAPI_key from "./config.json"; // FIXME: 가져오기 오류
+// import { readFileSync } from "fs";
+import { openAPI_key } from "./config.json"; // FIXME: 가져오기 오류
 
-let openAPI_key = JSON.parse(readFileSync("./config.json", "utf-8"))["openAPI_key"];
+// let openAPI_key = JSON.parse(readFileSync("./config.json", "utf-8"))["openAPI_key"];
+
+export class NotFoundError extends Error {};
+export class ServerError   extends Error {};
+export class InvalidKey    extends Error {};
+
+TypeError
 
 class OpenAPI {
-    static async getSchoolCode(school_name: string): Promise<string> {
+    static getSchoolCode(school_name: string): Promise<string> {
         let qs: string = new URLSearchParams({
             "KEY": openAPI_key.toString(), 
             "Type": "json", 
             "SCHUL_NM": school_name,
         }).toString();
 
-        let res = await fetch("https://open.neis.go.kr/hub/schoolInfo?"+qs);
-        let data = JSON.parse(await res.text());
+        let response_data;
 
-        if (data.result.code === "INFO-200")
-            return "ERR_NF";
-        if (data.result.code === "ERROR-500")
-            return "ERR_SE";
-        
-        return data.schoolInfo[1].row[0].SD_SCHUL_CODE;
+        return fetch("https://open.neis.go.kr/hub/schoolInfo?"+qs)
+            .then(data => data.json())
+            .then(data => {
+                response_data = data;
+
+                return data.schoolInfo[1].row[0].SD_SCHUL_CODE;
+            })
+            .catch(err => {
+                // FIXME: 오류 처리
+                if (response_data.RESULT.CODE === "INFO-200")
+                    throw NotFoundError;
+                else if (response_data.RESULT.CODE === "ERROR-500")
+                    throw ServerError;
+                else if (response_data.RESULT.CODE === "ERROR-290")
+                    throw InvalidKey;
+            });
     }
 }
 
@@ -29,4 +44,6 @@ class DatabaseIO {
 
 }
 
-export { OpenAPI, DatabaseIO }
+export { 
+    OpenAPI, DatabaseIO
+}
